cfbe182bc79ed0ce1086e1aa6804705cfe04052a
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-05-06 10:57:39 +0200

    integrate the video tutorials / showcase in documentation/index.page

--- ext/rock.rb
+++ ext/rock.rb
@@ -1,5 +1,23 @@
 require 'webgen/tag'
 
+class Vimeo
+    include Webgen::Tag::Base
+
+    def call(tag, body, context)
+        id = param('vimeo.video_id')
+        width = param('vimeo.width')
+        height = param('vimeo.height')
+        embed = param('vimeo.embed')
+        iframe = %{<iframe src="http://player.vimeo.com/video/#{id}" width="#{width}" height="#{height}" frameborder="0" webkitAllowFullScreen="1" mozallowfullscreen="1" allowFullScreen="1">Vimeo Video</iframe>}
+        if embed
+            %{\n<p class="embed" style="width: #{width}px;" markdown="0">\n#{iframe}\n</p>\n}
+        else
+            iframe
+        end
+    end
+end
+
+
 class RockType
     include Webgen::Tag::Base
 
@@ -78,4 +96,8 @@ config.rocksource.url nil, :mandatory => 'default'
 config['contentprocessor.tags.map']['rock_source'] = 'RockSource'
 config.rockapi.pkg_name nil, :mandatory => 'default'
 config['contentprocessor.tags.map']['rock_api'] = 'RockAPI'
-
+config.vimeo.video_id 0, :mandatory => 'default'
+config.vimeo.width 250
+config.vimeo.height 180
+config.vimeo.embed true
+config['contentprocessor.tags.map']['vimeo'] = 'Vimeo'
